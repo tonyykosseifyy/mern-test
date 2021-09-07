c635282ce1cc98b4e8159a919a427c3ed5ecf9ab
tonyy kosseigyy <tony kosseify>
2021-09-07 10:50:11 +0300

    connecting with mongodb

--- index.js
+++ index.js
@@ -6,9 +6,22 @@ const app = express() ;
 const port = process.env.DB_HOST ;
 const morgan = require("morgan") ;
 const authRoutes = require("./routes/auth")
-
+const { MongoClient } = require('mongodb');
+const db = require("mongodb") ;
 app.use(cors()) ;
 app.use(morgan("dev")) ;
+
+
+
+
+const uri = "mongodb+srv://new-user-03018765:03018765@cluster0.vtwtx.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  console.log("\nDB CONNECTED...")
+
+  // perform actions on the collection object
+  client.close();
+});
 
 
 
