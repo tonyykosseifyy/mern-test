6ba23655d2ca709a8321159215a63f16fbee5cf1
tonyy kosseigyy <tonykosseify123@gmail.com>
2021-09-11 17:57:38 +0300

    remove testing console.logs

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,32 +1,33 @@
-import User from '../models/user';
-import { hashPassword , comparePassword } from '../helpers/auth' ;
+import User from "../models/user";
+import { hashPassword, comparePassword } from "../helpers/auth";
 
 export const register = async (req, res) => {
-  const { name , email , password , secret } = req.body ;
+  const { name, email, password, secret } = req.body;
 
-  if(!name) return res.status(400).send("Name is required") ;
-  console.log('name is here')
-  console.log(`password: ${password}`)
-  if(!password || password.length < 6 ) return res.status(400).send("Password is required and should be 6 characters long")
+  if (!name) return res.status(400).send("Name is required");
 
-  if(!secret) return res.status(400).send("Answer is required") ;
-    console.log('secret is here')
-    if ( !email ) return res.status(400).send("Email is required") ;
-    
-    const exist = await User.findOne({ email }) ;
-    if (exist) return res.status(400).send("Email is taken");
-    console.log('email not taked ')
-    const hashedPassword = await hashPassword(password) ;
+  if (!password || password.length < 6)
+    return res
+      .status(400)
+      .send("Password is required and should be 6 characters long");
 
-    const user = new User({name , email , password: hashedPassword ,secret }) ;
+  if (!secret) return res.status(400).send("Answer is required");
 
-    try {
-      await user.save()
-      console.log("Saved")
-      return res.json({ok: true})
-    } catch(err) {
-      console.log(err)
-      return res.status(400).send("Error, Try again") ;
-    }
+  if (!email) return res.status(400).send("Email is required");
 
+  const exist = await User.findOne({ email });
+  if (exist) return res.status(400).send("Email is taken");
+
+  const hashedPassword = await hashPassword(password);
+
+  const user = new User({ name, email, password: hashedPassword, secret });
+
+  try {
+    await user.save();
+    console.log("Saved");
+    return res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error, Try again");
+  }
 };
